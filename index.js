391bd8101b24dc30082542d8219f95d690b030a7
Metz Dániel <daniel.metz@initon.com>
2020-10-17 20:26:53 +0200

    re-initializing on window resize

--- index.js
+++ index.js
@@ -2,37 +2,43 @@ import Vec2 from "./Vec2.js";
 import ParticleSystem from "./ParticleSystem.js";
 
 const canvas = document.getElementById("canvas");
-canvas.width =  canvas.style.width = innerWidth;
-canvas.height = canvas.style.height = innerHeight;
-
-const system = new ParticleSystem(canvas);
-
-canvas.addEventListener("click",function(){
-	system.dropStone();
-});
-
-canvas.addEventListener("contextmenu",function(e){
-	e.preventDefault();
-	system.dropFluid();
-});
-
-document.addEventListener("keypress",function(e){
-	if (e.key=='+' && dtime<0.1) {
-		dtime *= 2.0;
+let afReq = null;
+window.onload = window.onresize = function() {
+	if (afReq!=null) {
+		cancelAnimationFrame(afReq);
+		afReq = null;
 	}
-	if (e.key=='-') {
-		dtime /= 2.0;
-	}
-	if (e.key=='c') {
-		system.cool();
-	}
-});
+	canvas.width =  canvas.style.width = innerWidth;
+	canvas.height = canvas.style.height = innerHeight;
+	const system = new ParticleSystem(canvas);
 
-var dtime = 0.1;
-function tick() {
-	system.update(dtime);
-	system.render(dtime);
-	requestAnimationFrame(tick);
+	canvas.addEventListener("click",function(){
+		system.dropStone();
+	});
+
+	canvas.addEventListener("contextmenu",function(e){
+		e.preventDefault();
+		system.dropFluid();
+	});
+
+	document.addEventListener("keypress",function(e){
+		if (e.key=='+' && dtime<0.1) {
+			dtime *= 2.0;
+		}
+		if (e.key=='-') {
+			dtime /= 2.0;
+		}
+		if (e.key=='c') {
+			system.cool();
+		}
+	});
+
+	var dtime = 0.1;
+	function tick() {
+		system.update(dtime);
+		system.render(dtime);
+		afReq = requestAnimationFrame(tick);
+	}
+
+	tick();
 }
-
-tick();
